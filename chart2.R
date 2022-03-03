cf53fc719bb76d236701b3fea2eca0488b7f2f54
unknown <planet-zg>
2022-03-03 01:45:55 -0800

    edit chart1 chart2, report

--- chart2.R
+++ chart2.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 trends <- read.csv("incarceration_trends.csv")
 trends <-  trends %>% filter(!is.na(female_prison_adm_rate),
-                             !is.na(male_prison_pop_rate)) 
+                             !is.na(male_prison_adm_rate)) 
 
 trends %>% group_by(year) %>% summarise(female=mean(female_prison_adm_rate),
                                         male=mean(male_prison_adm_rate)) %>% 
